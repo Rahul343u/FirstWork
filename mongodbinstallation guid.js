60e311fe7cfe523d2d9319168027e59591585332
Rahul343u <33927822+Rahul343u@users.noreply.github.com>
2017-11-28 23:21:02 +0530

    Add files via upload

---
+++ mongodbinstallation guid.js
@@ -0,0 +1,18 @@
+important steps for mongodb
+
+C:\>md data
+C:\md data\db
+
+If you have to install the MongoDB at a different location, then you need to specify an 
+alternate path for \data\db by setting the path dbpath in mongod.exe. For the same, issue the following commands.
+
+D:\set up\mongodb\bin>mongod.exe --dbpath "d:\set up\mongodb\data" 
+D:\set up\mongodb\bin>mongo.exe
+
+http://technoon.github.io/lessons/mongodb-nodejs
+
+http://www.transistor.io/getting-started-with-node-express-and-mongodb.html
+
+https://evdokimovm.github.io/javascript/nodejs/mongodb/pagination/expressjs/ejs/bootstrap/2017/08/20/create-pagination-with-nodejs-mongodb-express-and-ejs-step-by-step-from-scratch.html
+
+https://scotch.io/tutorials/setting-up-a-mean-stack-single-page-application
