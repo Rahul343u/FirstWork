08e45b52d7246407b978dfaff17c8d025185719c
Rahul343u <33927822+Rahul343u@users.noreply.github.com>
2017-11-29 11:18:36 +0530

    Update mongodbinstallation guid.js

--- mongodbinstallation guid.js
+++ mongodbinstallation guid.js
@@ -16,3 +16,4 @@ http://www.transistor.io/getting-started-with-node-express-and-mongodb.html
 https://evdokimovm.github.io/javascript/nodejs/mongodb/pagination/expressjs/ejs/bootstrap/2017/08/20/create-pagination-with-nodejs-mongodb-express-and-ejs-step-by-step-from-scratch.html
 
 https://scotch.io/tutorials/setting-up-a-mean-stack-single-page-application
+https://www.youtube.com/redirect?q=https%3A%2F%2Fgithub.com%2Fbradtraversy%2Fmean_mytasklist&redir_token=ippCIoE5wg4WUwe68UTQFXXiU558MTUxMjAxODE1NkAxNTExOTMxNzU2&v=PFP0oXNNveg&event=video_description
